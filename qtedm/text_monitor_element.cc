878cd5419808bb74574ac62578bef2b5601762a9
Robert Soliday <soliday@anl.gov>
2025-10-24 13:15:03 -0500

    Improving appearence

--- qtedm/text_monitor_element.cc
+++ qtedm/text_monitor_element.cc
@@ -36,7 +36,7 @@ TextMonitorElement::TextMonitorElement(QWidget *parent)
   setWordWrap(false);
   // Reduce margins to match MEDM text positioning and maximize text space
   // Top margin 0 for vertical alignment, left/right 1 for minimal padding
-  setContentsMargins(0, 0, 1, 2);
+  setContentsMargins(0, 0, 1, 1);
   setAttribute(Qt::WA_TransparentForMouseEvents);
   setTextAlignment(Qt::AlignLeft | Qt::AlignTop);
   setForegroundColor(defaultForegroundColor());

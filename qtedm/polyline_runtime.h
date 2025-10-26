8d1d47134b24661e7c957b6b325f01f60f38c717
Robert Soliday <soliday@anl.gov>
2025-10-26 15:42:23 -0500

    Simplifying code

--- qtedm/polyline_runtime.h
+++ qtedm/polyline_runtime.h
@@ -1,73 +1,14 @@
 #pragma once
 
-#include <array>
-
-#include <QByteArray>
-#include <QObject>
-#include <QString>
-
-#include <cadef.h>
+#include "graphic_element_runtime_base.h"
 
 class PolylineElement;
 
-class DisplayWindow;
-
-class PolylineRuntime : public QObject
+class PolylineRuntime : public GraphicElementRuntimeBase<PolylineElement>
 {
-  friend class DisplayWindow;
 public:
-  explicit PolylineRuntime(PolylineElement *element);
-  ~PolylineRuntime() override;
-
-  void start();
-  void stop();
-
-private:
-  struct ChannelRuntime
+  explicit PolylineRuntime(PolylineElement *element)
+    : GraphicElementRuntimeBase(element)
   {
-    PolylineRuntime *owner = nullptr;
-    int index = 0;
-    QString name;
-    chid channelId = nullptr;
-    evid subscriptionId = nullptr;
-    chtype subscriptionType = DBR_TIME_DOUBLE;
-    short fieldType = -1;
-    long elementCount = 1;
-    bool connected = false;
-    bool hasValue = false;
-    bool controlInfoRequested = false;
-    bool hasControlInfo = false;
-    double value = 0.0;
-    short severity = 0;
-    short status = 0;
-    double hopr = 0.0;
-    double lopr = 0.0;
-    short precision = -1;
-  };
-
-  void resetState();
-  void initializeChannels();
-  void cleanupChannels();
-  void subscribeChannel(ChannelRuntime &channel);
-  void unsubscribeChannel(ChannelRuntime &channel);
-  void requestControlInfo(ChannelRuntime &channel);
-  void handleChannelConnection(ChannelRuntime &channel,
-      const connection_handler_args &args);
-  void handleChannelValue(ChannelRuntime &channel,
-      const event_handler_args &args);
-  void handleChannelControlInfo(ChannelRuntime &channel,
-      const event_handler_args &args);
-  void evaluateState();
-  bool evaluateCalcExpression(double &result) const;
-
-  static void channelConnectionCallback(struct connection_handler_args args);
-  static void valueEventCallback(struct event_handler_args args);
-  static void controlInfoCallback(struct event_handler_args args);
-
-  PolylineElement *element_ = nullptr;
-  std::array<ChannelRuntime, 5> channels_{};
-  QByteArray calcPostfix_;
-  bool calcValid_ = false;
-  bool channelsNeeded_ = true;
-  bool started_ = false;
+  }
 };

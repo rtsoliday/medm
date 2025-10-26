3ec28ba2ec71056e4c84ce35bc2b94caeae1d072
Robert Soliday <soliday@anl.gov>
2025-10-26 15:51:18 -0500

    Simplifying code

--- qtedm/bar_monitor_runtime.h
+++ qtedm/bar_monitor_runtime.h
@@ -1,70 +1,14 @@
 #pragma once
 
-#include <QObject>
-#include <QPointer>
-#include <QMetaObject>
-#include <QString>
-
-#include <utility>
-
-#include <cadef.h>
+#include "single_channel_monitor_runtime_base.h"
 
 class BarMonitorElement;
 
-class DisplayWindow;
-
-class BarMonitorRuntime : public QObject
+class BarMonitorRuntime : public SingleChannelMonitorRuntimeBase<BarMonitorElement>
 {
-  friend class DisplayWindow;
 public:
-  explicit BarMonitorRuntime(BarMonitorElement *element);
-  ~BarMonitorRuntime() override;
-
-  void start();
-  void stop();
-
-private:
-  void resetRuntimeState();
-  void subscribe();
-  void unsubscribe();
-  void requestControlInfo();
-  void handleConnectionEvent(const connection_handler_args &args);
-  void handleValueEvent(const event_handler_args &args);
-  void handleControlInfo(const event_handler_args &args);
-
-  template <typename Func>
-  void invokeOnElement(Func &&func);
-
-  static void channelConnectionCallback(struct connection_handler_args args);
-  static void valueEventCallback(struct event_handler_args args);
-  static void controlInfoCallback(struct event_handler_args args);
-
-  QPointer<BarMonitorElement> element_;
-  QString channelName_;
-  chid channelId_ = nullptr;
-  evid subscriptionId_ = nullptr;
-  bool started_ = false;
-  bool connected_ = false;
-  short fieldType_ = -1;
-  long elementCount_ = 1;
-  double lastValue_ = 0.0;
-  bool hasLastValue_ = false;
-  short lastSeverity_ = 3;
-};
-
-template <typename Func>
-inline void BarMonitorRuntime::invokeOnElement(Func &&func)
-{
-  if (!element_) {
-    return;
+  explicit BarMonitorRuntime(BarMonitorElement *element)
+    : SingleChannelMonitorRuntimeBase(element)
+  {
   }
-  QPointer<BarMonitorElement> target = element_;
-  QMetaObject::invokeMethod(element_.data(),
-      [target, func = std::forward<Func>(func)]() mutable {
-        if (!target) {
-          return;
-        }
-        func(target.data());
-      },
-      Qt::QueuedConnection);
-}
+};
